a0829b26cd7ad5474b996308b1e4e4cbceb6996f
«Artur» <art13471118@gmail.com>
2022-12-30 12:48:38 +0300

    Рекурсия.Программа выводит все четные числа в заданном диапазоне

---
+++ Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все
+// четные числа в промежутке от M до N.
+// M = 1; N = 5. -> "2,4"
+// M = 4; N = 8. -> "4, 6, 8"
+
+Console.WriteLine("M");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("N");
+int N = int.Parse(Console.ReadLine()!);
+
+string Rec(int a, int b)
+{
+    while(a%2!=0) a++;
+    if (a%2==0 && a<=b) return $"{a} "+ Rec(a+1, b);
+    else return String.Empty;
+}
+Console.WriteLine(Natural(M,N));
+Console.WriteLine(Rec(M,N));
