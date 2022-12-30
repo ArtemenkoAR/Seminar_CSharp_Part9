66a849375d4ae793c12a28655dcfacc385451a7a
«Artur» <art13471118@gmail.com>
2022-12-30 13:29:02 +0300

    Рекурсия.Программа находит сумму натуральных элементов в промежутке заданных чисел

---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("M");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("N");
+int N = int.Parse(Console.ReadLine()!);
+
+int Rec(int a, int b)
+{
+    while(a<1) a++;
+    if (a<=b) return a+ Rec(a+1, b);
+    else return 0;
+}
+Console.WriteLine(Rec(M,N));
